0ab9c251f6067aac72bb4f8e6a78c11cf5ffe808
Joseph Lai <joe5621624@gmail.com>
2023-01-12 21:07:52 +0800

    update

--- R/InputDATA.R
+++ R/InputDATA.R
@@ -14,7 +14,7 @@ bedfromfile <- function(data, name="name", strand=NULL){
   if (is.data.frame(data)){
     table <- data
   } else if (file.exists(data)){
-    table <- read.table(data, header=FALSE)
+    table <- readr::read_tsv(data, col_names=FALSE)
   } else {
     stop("Check the input data format or check the file exsist in path")
   }
