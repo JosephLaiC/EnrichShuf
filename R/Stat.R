a6fc5dccebf7c9a54646560ec48354d9ba40edd9
Joseph Lai <35094752+JosephLaiC@users.noreply.github.com>
2023-12-08 09:51:26 +0800

    Update Stat.R

--- R/Stat.R
+++ R/Stat.R
@@ -202,7 +202,7 @@ TargetFactorSTAT <- function(
   ## Randomly select elements from total
   if (parallel==1) {
 
-    expect.num <- expect.num <- sapply(1:random.num, function(x) 
+    expect.num <- sapply(1:random.num, function(x) 
       { sum(total_dat[randomFactor(total_idx, seed=x, n=feature_len_num),"target"]) })
 
   } else if (parallel > 1) {
